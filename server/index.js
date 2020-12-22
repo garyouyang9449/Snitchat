037550d150ebeec5529529bfda4732c808ac0fbe
garyouyang9449 <garyouyang9449@gmail.com>
2020-12-21 16:43:00 -0800

    added index.js in /server

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+const cors = require('cors');
+
+// const { addUesr, removeUser, getUser, getUsersInRoom } = require('./users.js');
+
+const PORT = process.env.PORT || 5000;
+
+// const router = require('./router');
+
+// instantiate sock.io server
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+//app.use(router); 
+app.use(cors());
+
+server.listen(PORT, () => console.log(`server has started on port ${PORT}`));
