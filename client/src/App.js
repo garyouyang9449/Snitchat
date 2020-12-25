38b278e56c0f9e14df23b6323073545a80cc036b
garyouyang9449 <garyouyang9449@gmail.com>
2020-12-24 16:05:36 -0800

    redesigned the project.

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,13 @@ import React from 'react';
  
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-// import necessary components
-import Join from './components/Join/Join';
-import Topics from './components/Topics/Topics';
+// import necessary componentsRooms
+import Rooms from './components/Rooms/Rooms';
 
 // Define the structure of the web app
 const App = () => (
     <Router>
-        <Route path="/" exact component={Join} /> 
-        <Route path="/topics" exact component={Topics} /> 
+        <Route path="/" exact component={Rooms} /> 
     </Router>
 );
 
---
+++ client/src/components/Rooms/Rooms.js
@@ -0,0 +1,21 @@
+/*
+This file is the Join page, where the users enter their and will be redirected to the topics page
+ */
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Rooms = () => {
+    const [school, setSchool] = useState(''); 
+
+    return (
+        <div>
+            <div><input placeholder="School Name..." className="joinInput" type="text" onChange={(event) => setSchool(event.target.value)} /></div>
+            <Link onClick={event => (!school) ? event.preventDefault : null} to={`/chat?school=${school}`}>
+                <button className="button mt-20" type="submit">Join</button>
+            </Link>
+        </div>
+        
+    )
+}; 
+
+export default Rooms;
